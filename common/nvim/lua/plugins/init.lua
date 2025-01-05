c744742e1cbc34af6e183b13502eed774d114aa5
555isfaiz <boyuan.xiao_aka_mark@outlook.com>
2025-01-05 16:05:41 +0100

    fix windows.nvim

--- common/nvim/lua/plugins/init.lua
+++ common/nvim/lua/plugins/init.lua
@@ -62,6 +62,12 @@ return {
             "anuvyklack/middleclass",
             "anuvyklack/animation.nvim"
         },
+        config = function()
+            vim.o.winwidth = 10
+            vim.o.winminwidth = 10
+            vim.o.equalalways = false
+            require('windows').setup()
+        end
     },
     {
         'mfussenegger/nvim-jdtls',
