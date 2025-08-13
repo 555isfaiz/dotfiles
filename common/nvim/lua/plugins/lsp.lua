83f7ddcff4ea3dd19b8157ff30fe96242602cfce
555isfaiz <boyuan.xiao_aka_mark@outlook.com>
2025-08-13 09:59:44 +0200

    setup linter and formatter

--- common/nvim/lua/plugins/lsp.lua
+++ common/nvim/lua/plugins/lsp.lua
@@ -120,8 +120,7 @@ return {
         config = function()
             require("conform").setup({
                 formatters_by_ft = {
-                    vim = { "vint" },
-                    sql = { "sql_formatter" },
+                    sql = { "sqlfmt" },
                     -- -- Conform will run multiple formatters sequentially
                     -- python = { "isort", "black" },
                     -- -- You can customize some of the format options for the filetype (:help conform.format)
--- common/nvim/lua/plugins/mason.lua
+++ common/nvim/lua/plugins/mason.lua
@@ -6,6 +6,27 @@ return {
         end
     },
     {
+        "zapling/mason-conform.nvim",
+        dependencies = { 'williamboman/mason.nvim', 'stevearc/conform.nvim' },
+    },
+    {
+        "rshkarin/mason-nvim-lint",
+        dependencies = { "williamboman/mason.nvim", "mfussenegger/nvim-lint" },
+        config = function()
+            vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+                callback = function()
+                    -- try_lint without arguments runs the linters defined in `linters_by_ft`
+                    -- for the current filetype
+                    require("lint").try_lint()
+                end,
+            })
+            require('lint').linters_by_ft = {
+                markdown = { 'vale' },
+                sql = { 'sqlfluff' },
+            }
+        end
+    },
+    {
         'jay-babu/mason-nvim-dap.nvim',
         event = 'VeryLazy',
         config = function()
@@ -17,35 +38,33 @@ return {
         event = 'VeryLazy',
         config = function()
             require("mason-lspconfig").setup {
-                automatic_enable = false
+                automatic_enable = false,
+                -- The first entry (without a key) will be the default handler
+                -- and will be called for each installed server that doesn't have
+                -- a dedicated handler.
+                function(server_name) -- default handler (optional)
+                    require("lspconfig")[server_name].setup {
+                        capabilities = require("cmp_nvim_lsp").default_capabilities()
+                    }
+                end,
+                -- Next, you can provide a dedicated handler for specific servers.
+                -- For example, a handler override for the `rust_analyzer`:
+                -- ["pyright"] = function ()
+                --    require("lspconfig")["pyright"].setup {
+                --      capabilities = require("cmp_nvim_lsp").default_capabilities();
+                --      settings = {
+                --          python = {
+                --            analysis = {
+                --               typeCheckingMode = 'off'
+                --              }
+                --            }
+                --        }
+                --      }
+                -- end,
+                ["jdtls"] = function()
+                    -- The key is to do nothing here...
+                end
             }
-            -- require("mason-lspconfig").setup_handlers {
-            --     -- The first entry (without a key) will be the default handler
-            --     -- and will be called for each installed server that doesn't have
-            --     -- a dedicated handler.
-            --     function(server_name) -- default handler (optional)
-            --         require("lspconfig")[server_name].setup {
-            --             capabilities = require("cmp_nvim_lsp").default_capabilities()
-            --         }
-            --     end,
-            --     -- Next, you can provide a dedicated handler for specific servers.
-            --     -- For example, a handler override for the `rust_analyzer`:
-            --     -- ["pyright"] = function ()
-            --     --    require("lspconfig")["pyright"].setup {
-            --     --      capabilities = require("cmp_nvim_lsp").default_capabilities();
-            --     --      settings = {
-            --     --          python = {
-            --     --            analysis = {
-            --     --               typeCheckingMode = 'off'
-            --     --              }
-            --     --            }
-            --     --        }
-            --     --      }
-            --     -- end,
-            --     ["jdtls"] = function()
-            --         -- The key is to do nothing here...
-            --     end
-            -- }
         end
     },
 }
