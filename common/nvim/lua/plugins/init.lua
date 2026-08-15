8eec24d1446014842947573cff942229c8adc742
fu1lp0w3r <fu1lp0w3r@fu1lp0w3rs-Mac-mini.local>
2026-08-15 18:08:02 +0200

    add mermaid plugin

--- common/nvim/lua/plugins/init.lua
+++ common/nvim/lua/plugins/init.lua
@@ -10,6 +10,16 @@ return {
     'MunifTanjim/nui.nvim',
     'projekt0n/github-nvim-theme',
     {
+        "kevalin/mermaid.nvim",
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        config = function()
+            require("mermaid").setup()
+
+            -- Install the Tree-sitter parser:
+            -- :TSInstall mermaid
+        end,
+    },
+    {
         "Aejkatappaja/sora",
         lazy = false,
         priority = 1000,
